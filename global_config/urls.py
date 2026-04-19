770ccac9e20b2b9c21d7338174e019f5893fd32f
muhamedrar <mohamedahmed0218@gmail.com>
2026-04-19 17:54:58 +0200

    feat: added regester form and flash messages

--- global_config/urls.py
+++ global_config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users.views import register
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register),
     path('', include('blog.urls')),
 ]
