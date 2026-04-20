42676e937817026172e82edfc0113e05a6a07198
muhamedrar <mohamedahmed0218@gmail.com>
2026-04-20 18:14:09 +0200

    completed the auth system and added profile reqs

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from users.forms import registerationForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +9,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request=request, message= f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request=request, message= f'Your account has been created! you are now able to login')
+            return redirect('login')
     else:
         form = registerationForm()
 
     return render(request, template_name='users/register.html',context={"form":form})
+
+@login_required
+def profile(request):
+    return render(request=request, template_name='users/profile.html',context={'user': request.user})
