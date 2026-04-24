a4a02a1f2caf3d5b60ac9ae9cccbab659e15716c
muhamedrar <mohamedahmed0218@gmail.com>
2026-04-25 00:51:27 +0300

    feat: enhance user profile page with image and details
    Add user profile image and details to the profile page, improving the user experience and providing a more personalized interface.

--- global_config/urls.py
+++ global_config/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users.views import register , profile
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +30,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
 
     path('', include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
