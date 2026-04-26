9b369dce88093ebed4e4932a60a640dc57cd4bbd
muhamedrar <mohamedahmed0218@gmail.com>
2026-04-26 04:57:55 +0300

    feat(ui): enhance profile page layout and styling

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-
+from .models import Profile
 
 
 class registerationForm(UserCreationForm):
@@ -11,3 +11,17 @@ class registerationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email', 'password1', 'password2']
+
+
+class userUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+class userUpdateImage(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from users.forms import registerationForm
+from users.forms import registerationForm, userUpdateForm, userUpdateImage
 from django.contrib.auth.decorators import login_required
 from blog.models import Post
 
@@ -20,4 +20,27 @@ def register(request):
 @login_required
 def profile(request):
     user_posts = Post.objects.filter(author_id = request.user.id)
-    return render(request=request, template_name='users/profile.html',context={"user_posts": user_posts})
+
+    if request.method == 'POST':
+        u_form  = userUpdateForm(request.POST,instance=request.user)
+        p_form = userUpdateImage(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request=request, message= f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = userUpdateForm(instance=request.user)
+        p_form = userUpdateImage(instance=request.user.profile)
+
+
+    context = {
+        'user_posts': user_posts,
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request=request, template_name='users/profile.html',context=context)
