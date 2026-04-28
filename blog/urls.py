7c44fa0cc6380d7f067220ca644dedb4495f8bfe
muhamedrar <mohamedahmed0218@gmail.com>
2026-04-28 17:20:57 +0300

    feat(blog): implement PostListView for home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostViews
 
 urlpatterns =[
-    path('', view=views.home, name='blog-home'),
+    # path('', view=views.home, name='blog-home'),
+    path('', view=PostViews.as_view(), name='blog-home'),
     path('about/', view=views.about, name='blog-about'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
-
+from django.views.generic import ListView
 
 posts = Post.objects.all()
 
-# Create your views here.
-def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request=request,template_name='blog/home.html',context=context)
+
+# def home(request):
+#     context = {
+#         'posts': posts
+#     }
+#     return render(request=request,template_name='blog/home.html',context=context)
+
+
+class PostViews(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = '-data_posted'
 
 def about(request):
     return  render(request=request,template_name='blog/about.html',context={'title': 'About'})
